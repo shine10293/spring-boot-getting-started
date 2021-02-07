ff28b40efefe1ab77c5b527c355c4c745fdb0fa7
shin_seungki <shine10293@gmail.com>
2021-02-07 16:32:10 +0900

    connect hibernate

--- src/main/java/org/shin/App.java
+++ src/main/java/org/shin/App.java
@@ -1,13 +1,22 @@
 package org.shin;
 
-import com.apple.eawt.Application;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @SpringBootApplication
 public class App {
-
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
