1716ca92935066ba92a01131ed23c2b194b88756
shin_seungki <shine10293@gmail.com>
2021-02-09 23:23:37 +0900

    クラス名修正

--- src/main/java/org/shin/Common/CommonInterceptor.java
+++ src/main/java/org/shin/Common/CommonInterceptor.java
@@ -1,12 +1,18 @@
 package org.shin.Common;
 
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class CommonIntercepter extends HandlerInterceptorAdapter {
+public class CommonInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest req, HttpServletResponse res, Object handler) throws Exception {
+
         return true;
+    }
+
+    public void postHandle(HttpServletRequest req, HttpServletResponse res, Object handler, ModelAndView modelAndView) {
+
     }
 }
